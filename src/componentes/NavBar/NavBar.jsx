129e4593c0c123e4cce82c9e78e6c7398e373ca8
Natalia Palmisano <natytom188@gmail.com>
2025-05-23 20:41:13 -0300

    responsive navbar

--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -1,8 +1,15 @@
+import React, { useState } from 'react';
 import CartWidget from '../CartWidget/CartWidget';
-import "./NavBar.css";
 import { Link, NavLink } from 'react-router-dom';
+import "./NavBar.css";
 
 const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(prev => !prev);
+  };
+
   return (
     <header>
       <h1>El Diablo es Puerco</h1>
@@ -15,16 +22,18 @@ const NavBar = () => {
         />
       </Link>
 
-      <nav>
-  <ul>
-    <li><NavLink to="/categoria/novelas">Remeras Cine</NavLink></li>
-    <li><NavLink to="/categoria/infantiles">Remeras Infantiles</NavLink></li>
-    <li><NavLink to="/categoria/peliculas">Remeras Dibujitos</NavLink></li>
-    <li><NavLink to="/categoria/dibujitos">Remeras Series</NavLink></li>
-  </ul>
-</nav>
+      <button className="menu-toggle" onClick={toggleMenu}>
+        ☰
+      </button>
 
-
+      <nav className={menuOpen ? "open" : ""}>
+        <ul>
+          <li><NavLink to="/categoria/novelas" onClick={() => setMenuOpen(false)}>Remeras Cine</NavLink></li>
+          <li><NavLink to="/categoria/infantiles" onClick={() => setMenuOpen(false)}>Remeras Infantiles</NavLink></li>
+          <li><NavLink to="/categoria/peliculas" onClick={() => setMenuOpen(false)}>Remeras Dibujitos</NavLink></li>
+          <li><NavLink to="/categoria/dibujitos" onClick={() => setMenuOpen(false)}>Remeras Series</NavLink></li>
+        </ul>
+      </nav>
 
       <CartWidget />
     </header>
